7e71c9f5263d16abde2d602a91a3e6e48b83c8c5
dingge2016 <dingy8@foxmail.com>
2019-10-31 17:11:08 -0700

    add AI

--- halma/views.py
+++ halma/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from halma.serializers import StateSerializer
+from rest_framework import generics
 # Create your views here.
 from django.http import HttpResponse
+from halma.models import State, Action
 
 
 def board(request):
-    return render(request, 'halma/board.html')
+    return render(request, 'halma/board.js')
+
+
+class StateList(generics.ListCreateAPIView):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
